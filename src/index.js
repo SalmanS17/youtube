eb117aebf83690e36bd3a0b2aef6fc6f03797ad6
Salman Khan <salmankhanabd343@gmail.com>
2025-01-16 11:47:51 +0500

    connected to mongodb

---
+++ src/index.js
@@ -0,0 +1,46 @@
+//  require ('dotenv').config()
+import constants from 'constants'
+// import mongoose from "mongoose";
+// import express from "express";
+import connectDb from './db/db.js'
+
+
+
+connectDb();
+
+
+
+
+
+
+
+
+
+// const app = express(); 
+// (async () => {
+//     try {
+//         // Use process.env.DB_URL and DB_Name correctly
+//         const DB_URL = process.env.DB_URL;
+//         const DB_Name = process.env.DB_Name;
+
+//         await mongoose.connect(`${DB_URL}/${DB_Name}`, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log("Connected to MongoDB!");
+
+//         // Handle app-level errors
+//         app.on("error", (error) => {
+//             console.error("App Error:", error);
+//         });
+
+//         // Start the server
+//         const port = process.env.PORT || 3000; // Default to port 3000 if not defined
+//         app.listen(port, () => {
+//             console.log(`App is listening on port ${port}`);
+//         });
+//     } catch (error) {
+//         console.error("Error starting the application:", error);
+//         process.exit(1); // Exit the process on error
+//     }
+// })();
